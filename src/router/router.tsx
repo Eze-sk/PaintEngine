f626f34f82bf7ef78522742b58b1c0b0cfb9d110
Eze-sk <ezequielsmiak@gmail.com>
2026-06-22 17:12:42 -0300

    feat: simplify router configuration by removing unused loader and random ID generation

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,27 +1,12 @@
-import { createBrowserRouter, redirect } from "react-router";
-import { customRandom, urlAlphabet } from "nanoid";
-import seedrandom from "seedrandom";
+import { createBrowserRouter } from "react-router";
 
 import EditorPage from "@pages/Editor/Page";
-
-const rng = seedrandom("url");
+import HomePage from "@pages/Home/Page";
 
 export const router = createBrowserRouter([
   {
-    path: "/editor/",
-    loader: async () => {
-      try {
-        const nanoID = customRandom(urlAlphabet, 3, (size) => {
-          return new Uint8Array(size).map(() => Math.floor(256 * rng()));
-        });
-
-        return redirect(`/editor/draft-${nanoID()}`);
-      } catch (err) {
-        throw new Error("Engine start error", {
-          cause: err,
-        });
-      }
-    },
+    path: "/editor",
+    element: <HomePage />,
   },
   {
     path: "/editor/:project_name",
