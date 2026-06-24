e4b32657e41bcc30564a9345bac714b995ba45b5
Eze-sk <ezequielsmiak@gmail.com>
2026-06-24 16:02:21 -0300

    Refactor: Simplify component by removing all rendering conditions and props, formatting and linting using biome

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,44 +1,17 @@
 import { CardWrapper } from "@components/layout/CardWrapper";
-import TabContainer from "../../pages/Editor/components/TabContainer";
+import TabContainer from "@components/ui/TabContainer";
 import UserProfile from "@components/ui/UserProfile";
-import Logo from "@components/ui/Logo";
 
-type TypeDistribution = "start" | "between" | "end";
-
-interface TypeProps {
-  hasTabs?: boolean;
-  hasUserProfile?: boolean;
-  hasLogo?: boolean;
-  distribution?: TypeDistribution;
-}
-
-export default function Header({
-  hasLogo,
-  hasTabs,
-  hasUserProfile,
-  distribution,
-}: TypeProps) {
-  const variantDistribution: Record<TypeDistribution, string> = {
-    start: "justify-start",
-    between: "justify-between",
-    end: "justify-end",
-  };
-
-  const getDistribution = variantDistribution[distribution ?? "between"];
-
-  return (
-    <CardWrapper
-      variant="blue"
-      gradientStyle="linear"
-      tag="header"
-      className={`
-        w-full px-2 rounded-t min-h-9.5
-        flex items-center ${getDistribution}
-      `}
-    >
-      {hasLogo ? <Logo /> : " "}
-      {hasTabs ? <TabContainer /> : " "}
-      {hasUserProfile ? <UserProfile /> : " "}
-    </CardWrapper>
-  );
+export default function Header() {
+	return (
+		<CardWrapper
+			variant="blue"
+			gradientStyle="linear"
+			tag="header"
+			className="w-full px-2 rounded-t min-h-9.5 flex items-center justify-between"
+		>
+			<TabContainer />
+			<UserProfile />
+		</CardWrapper>
+	);
 }
