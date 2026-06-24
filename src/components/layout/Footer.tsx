83b93f35c690d9ab62aeee435a4cdb6624a980e5
Eze-sk <ezequielsmiak@gmail.com>
2026-06-24 15:55:18 -0300

    feat & refactor: Add props className, formatting and lint using biome

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,18 @@
 import { CardWrapper } from "@components/layout/CardWrapper";
 
-export default function Footer() {
-  return (
-    <CardWrapper gradientStyle="linear" variant="cream" className="w-full px-2">
-      <span className="text-sm">Text footer</span>
-    </CardWrapper>
-  );
+interface TypeProps {
+	className?: string;
+}
+
+export default function Footer({ className }: TypeProps) {
+	return (
+		<CardWrapper
+			gradientStyle="linear"
+			variant="cream"
+			tag="footer"
+			className={`w-full px-2 ${className}`}
+		>
+			<span className="text-sm">Text footer</span>
+		</CardWrapper>
+	);
 }
