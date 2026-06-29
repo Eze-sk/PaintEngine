06ee0a0a792e9772f25c255bb92408186c2c2dd5
Eze-sk <ezequielsmiak@gmail.com>
2026-06-28 22:33:38 -0300

    refactor: update Header component structure and improve layout with NavLink

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,37 @@
 import { CardWrapper } from "@components/layout/CardWrapper";
 import TabContainer from "@components/ui/TabContainer";
 import UserProfile from "@components/ui/UserProfile";
+import { NavLink } from "react-router";
 
 export default function Header() {
-	return (
-		<CardWrapper
-			variant="blue"
-			gradientStyle="linear"
-			tag="header"
-			className="w-full px-2 rounded-t min-h-9.5 flex items-center justify-between"
-		>
-			<TabContainer />
-			<UserProfile />
-		</CardWrapper>
-	);
+  return (
+    <CardWrapper
+      variant="blue"
+      shadingStyle="linear"
+      tag="header"
+      className="w-full rounded-t-md flex min-h-9.5 overflow-hidden"
+    >
+      <NavLink to="/editor" end>
+        <CardWrapper
+          shadingStyle="square"
+          variant="green"
+          className="flex items-center h-full pl-2.5 gap-2 min-w-32.5 rounded-r-2xl rounded-tl-md"
+        >
+          <img
+            src="/assets/home.webp"
+            alt="Icon of a small house with a white facade and an orange roof"
+            width={30}
+            height={30}
+          />
+          <span className="text-white font-bold shadown text-shadow-lg">
+            Home
+          </span>
+        </CardWrapper>
+      </NavLink>
+      <div className="flex px-2 items-center justify-between w-full">
+        <TabContainer />
+        <UserProfile />
+      </div>
+    </CardWrapper>
+  );
 }
