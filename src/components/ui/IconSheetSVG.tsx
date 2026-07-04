de5adc45e62569d76942dfe13d02a3bcb05b5f7f
Eze-sk <ezequielsmiak@gmail.com>
2026-07-04 10:04:14 -0300

    feat: add IconSheetImage and IconSheetSVG components for enhanced icon rendering

--- src/components/ui/IconSheetSVG.tsx
+++ src/components/ui/IconSheetSVG.tsx
@@ -1,13 +1,19 @@
 import type { HTMLAttributes } from "react";
 
-type TypeIcon = "drop-down" | "filter" | "search";
+type TypeIcon =
+	| "drop-down"
+	| "filter"
+	| "search"
+	| "minimize"
+	| "expand"
+	| "close";
 
 type TypeProps = HTMLAttributes<HTMLOrSVGElement> & {
 	icon: TypeIcon;
 	size: number;
 };
 
-export default function SvgWrapper({ icon, size, ...props }: TypeProps) {
+export default function IconSheetSVG({ icon, size, ...props }: TypeProps) {
 	return (
 		<svg {...props} aria-hidden="true" width={size} height={size}>
 			<use href={`/assets/interfaceSpriteSheet.svg#${icon}`} />
