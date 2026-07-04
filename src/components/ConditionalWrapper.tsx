3e9da51632bed4a40529ee72cff391f57b534363
Eze-sk <ezequielsmiak@gmail.com>
2026-07-04 10:02:22 -0300

    fix: enhance ConditionalWrapper to support custom default tags

--- src/components/ConditionalWrapper.tsx
+++ src/components/ConditionalWrapper.tsx
@@ -1,13 +1,18 @@
-interface TypeProps {
+import type { AllowedTags } from "@/types/AllowedTagst";
+
+interface TypeProps<T extends AllowedTags> {
 	condition: boolean;
 	wrapper: (children: React.ReactNode) => React.JSX.Element;
 	children: React.ReactNode;
+	defaultTag?: T;
 }
 
-export default function ConditionalWrapper({
+export default function ConditionalWrapper<T extends AllowedTags = "div">({
 	condition,
 	wrapper,
 	children,
-}: TypeProps) {
-	return condition ? wrapper(children) : <div>{children}</div>;
+	defaultTag,
+}: TypeProps<T>) {
+	const Component = defaultTag || ("div" as React.ElementType);
+	return condition ? wrapper(children) : <Component>{children}</Component>;
 }
