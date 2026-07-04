4f77854f41f3c6061213df4fdac52dc4e6da69d3
Eze-sk <ezequielsmiak@gmail.com>
2026-07-04 10:05:14 -0300

    feat: enhance Checkbox component with additional label properties for improved accessibility

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,14 +1,23 @@
 interface TypeProps {
-	label: string;
-	className?: string;
+  label: string;
+  labelSrOnly?: boolean;
+  labelClass?: string;
+  className?: string;
 }
 
-export default function Checkbox({ label, className }: TypeProps) {
-	return (
-		<label className={`custom-checkbox ${className}`}>
-			<input type="checkbox" className="sr-only" id={`check-${label}`} />
-			<span className="sr-only">{label}</span>
-			<span className="checkbox-box" />
-		</label>
-	);
+export default function Checkbox({
+  label,
+  className,
+  labelSrOnly = false,
+  labelClass,
+}: TypeProps) {
+  return (
+    <label className={`custom-checkbox ${className ?? ""}`}>
+      <input type="checkbox" className="sr-only" id={`check-${label}`} />
+      <span className={`${labelSrOnly ? "sr-only" : ""} ${labelClass ?? ""}`}>
+        {label}
+      </span>
+      <span className="checkbox-box" />
+    </label>
+  );
 }
